11972c1ab5bb390f60c7b6ee8f64d43616241d0e
Lee Miller <iarkeus@gmail.com>
2013-02-25 21:27:31 -0800

    adding missing file

---
+++ lib/retention.rb
@@ -0,0 +1,26 @@
+class Retention
+  attr_reader :name, :time, :code
+  
+  def initialize(name, time, code)
+    @name = name
+    @time = time
+    @code = code
+  end
+  
+  def self.from_i(int)
+    return case int
+      when 1 then return HOUR
+      when 2 then return DAY
+      when 3 then return WEEK
+      when 4 then return MONTH
+      else return HOUR
+    end
+  end
+  
+  HOUR = Retention.new("Hour", 1.hour, "h").freeze
+  DAY = Retention.new("Day", 1.day, "d").freeze
+  WEEK = Retention.new("Week", 1.week, "w").freeze
+  MONTH = Retention.new("Month", 1.month, "m").freeze
+  
+  ALL = [HOUR, DAY, WEEK, MONTH].freeze
+end
